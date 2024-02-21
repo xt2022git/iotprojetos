889b2e5bdaf09c952941ba647f2551e33366b25d
José Williams Dantas Queiroga <xt2022wd@gmail.com>
2024-02-21 18:57:44 -0300

    sub a dist

---
+++ dist/assets/UserOrdersTab.cd47d8d5.js
@@ -0,0 +1,1 @@
+import{a as e,j as s}from"./index.166e41ba.js";const a=()=>e("div",{className:"loader",style:{minHeight:"80vh"},children:[s("h3",{children:"My Orders"}),s("strong",{children:s("span",{className:"text-subtle",children:"You don't have any orders"})})]});export{a as default};
---
+++ dist/assets/UserWishListTab.50e2c509.js
@@ -0,0 +1,1 @@
+import{a as e,j as s}from"./index.166e41ba.js";const i=()=>e("div",{className:"loader",style:{minHeight:"80vh"},children:[s("h3",{children:"My Wish List"}),s("strong",{children:s("span",{className:"text-subtle",children:"You don't have a wish list"})})]});export{i as default};
