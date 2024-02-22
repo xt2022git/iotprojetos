e5f65fea1bc5d840ed76ccc3148529ea13553b3e
José Williams Dantas Queiroga <xt2022wd@gmail.com>
2024-02-22 12:16:41 -0300

    mudança vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 100000000
+  },
   plugins: [
     react(),
   ],
