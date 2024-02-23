7e30b2a19372bd4a8a4940ee36891727016c61bf
José Williams Dantas Queiroga <xt2022wd@gmail.com>
2024-02-23 11:02:29 -0300

    barra de pesquisa atualizado

--- src/views/search/index.jsx
+++ src/views/search/index.jsx
@@ -65,7 +65,7 @@ const Search = ({ match }) => {
   return (
     <main className="content">
       <div className="loader">
-        <h4>Searching Product...</h4>
+        <h4>Procurando produto aguarde...</h4>
         <br />
         <LoadingOutlined style={{ fontSize: '3rem' }} />
       </div>
